2d14d49005a18e4c238e1f3ff0c90b6885cae566
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-28 11:42:21 +0500

    Initial commit

---
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/BitrixFlowsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.bitrixflowsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitrixFlowsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitrixFlowsIntegrationApplication.class, args);
+    }
+
+}
