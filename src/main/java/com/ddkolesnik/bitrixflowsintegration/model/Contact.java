8627781c11a130e34666395885f9dd254682eb3c
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-29 10:58:20 +0500

    Updated model. Use hibernate for manage entities. Updated properties.

--- src/main/java/com/ddkolesnik/bitrixflowsintegration/model/Contact.java
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/model/Contact.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
@@ -12,21 +14,31 @@ import java.util.List;
  */
 
 @Data
+@Entity
+@NoArgsConstructor
+@Table(name = "bitrix_contact")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Contact {
 
+    @Id
+    @Column(name = "id")
     @JsonProperty("ID")
     private String id;
 
+    @Column(name = "contact_name")
     @JsonProperty("NAME")
     private String name;
 
+    @Column(name = "contact_second_name")
     @JsonProperty("SECOND_NAME")
     private String secondName;
 
+    @Column(name = "contact_last_name")
     @JsonProperty("LAST_NAME")
     private String lastName;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "contact_id")
     @JsonProperty("EMAIL")
     private List<Email> email;
 
--- src/main/java/com/ddkolesnik/bitrixflowsintegration/model/Email.java
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/model/Email.java
@@ -1,29 +1,45 @@
 package com.ddkolesnik.bitrixflowsintegration.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * @author Alexandr Stegnin
  */
 
 @Data
+@Entity
+@NoArgsConstructor
+@Table(name = "bitrix_contact_email")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Email {
 
+    @Id
+    @Column(name = "id")
     @JsonProperty("ID")
     private String id;
 
+    @Column(name = "value_type")
     @JsonProperty("VALUE_TYPE")
     private String valueType;
 
+    @Column(name = "value")
     @JsonProperty("VALUE")
     private String value;
 
+    @Column(name = "type_id")
     @JsonProperty("TYPE_ID")
     private String typeId;
+
+    @JsonIgnore
+    @ManyToOne
+    private Contact contact;
 
     @JsonCreator
     public Email(@JsonProperty("ID") String id,
