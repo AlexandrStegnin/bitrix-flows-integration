3593598fe38941fc51a860e93258c07c56c62657
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-29 14:20:04 +0500

    Added scheduled service for manipulating data from Bitrix API to DDK storage.

--- src/main/java/com/ddkolesnik/bitrixflowsintegration/BitrixFlowsIntegrationApplication.java
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/BitrixFlowsIntegrationApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-@PropertySource("classpath:private.properties")
+@EnableScheduling
 @SpringBootApplication
+@PropertySource("classpath:private.properties")
 public class BitrixFlowsIntegrationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/service/ScheduledService.java
@@ -0,0 +1,43 @@
+package com.ddkolesnik.bitrixflowsintegration.service;
+
+import com.ddkolesnik.bitrixflowsintegration.model.BitrixResult;
+import com.ddkolesnik.bitrixflowsintegration.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ * <p>
+ * Сервис, который получет информацию из API Bitrix и сохраняет её в базу данных ДДК
+ */
+
+@Service
+public class ScheduledService {
+
+    private final ContactService contactService;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContactServiceImpl.class);
+
+    @Autowired
+    public ScheduledService(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    /**
+     * Обновление информации по контактам из Bitrix
+     * запускается каждый день в 01:00
+     */
+    @Scheduled(cron = "${spring.config.cron.expression}")
+    public void scheduledGetContactsFromBitrix() {
+        BitrixResult bitrixResult = contactService.getBitrixResult(0);
+        List<Contact> contacts = contactService.getContactsList(bitrixResult);
+        contacts = contactService.saveContacts(contacts);
+        LOG.info("Saved contacts list: " + contacts);
+    }
+
+}
