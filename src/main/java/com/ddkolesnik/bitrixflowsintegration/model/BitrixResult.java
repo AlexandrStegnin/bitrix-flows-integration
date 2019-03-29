56876d0f92eab767b33cc3981825c02c0414725d
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-29 09:41:16 +0500

    Added bitrix result model

---
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/model/BitrixResult.java
@@ -0,0 +1,16 @@
+package com.ddkolesnik.bitrixflowsintegration.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+public class BitrixResult {
+
+    private List<Contact> result;
+
+}
