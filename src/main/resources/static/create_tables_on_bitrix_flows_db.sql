5b8ee64b65ce713648fd8ff44343f3701f3f1ce2
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-29 14:22:46 +0500

    Added scripts for create tables on bitrix_flows DB.

---
+++ src/main/resources/static/create_tables_on_bitrix_flows_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE bitrix_flows.bitrix_contact (
+	id varchar(10) NOT NULL,
+	contact_name varchar(100) NULL,
+	contact_second_name varchar(100) NULL,
+	contact_last_name varchar(100) NULL,
+	CONSTRAINT bitrix_contact_pk PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+CREATE TABLE bitrix_flows.bitrix_contact_email (
+	id varchar(10) NOT NULL,
+	value_type varchar(100) NULL,
+	value varchar(100) NULL,
+	type_id varchar(100) NULL,
+	contact_id varchar(10) NOT NULL,
+	CONSTRAINT bitrix_contact_email_pk PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+ALTER TABLE bitrix_flows.bitrix_contact_email ADD CONSTRAINT bitrix_contact_email_bitrix_contact_fk FOREIGN KEY (contact_id) REFERENCES bitrix_flows.bitrix_contact(id) ON DELETE CASCADE;
