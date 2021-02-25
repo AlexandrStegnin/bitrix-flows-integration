1ee69c2a3508d5f8282e49f4bb0d6fdc775684a5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-02-25 13:22:25 +0500

    Добавлен перехват ошибок.

---
+++ src/main/java/com/ddkolesnik/bitrixflowsintegration/exception/ApiErrorResponse.java
@@ -0,0 +1,30 @@
+package com.ddkolesnik.bitrixflowsintegration.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Getter
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class ApiErrorResponse {
+
+    HttpStatus status;
+
+    String message;
+
+    Instant timestamp;
+
+    public ApiErrorResponse(HttpStatus status, String message, Instant timestamp) {
+        this.status= status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+}
